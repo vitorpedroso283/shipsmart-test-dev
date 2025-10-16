e302167f3cfcef001962ab7f862e111173636b7a
VITOR PEDROSO <vitorsamuel283@gmail.com>
2025-10-16 20:02:14 -0300

    chore: adicionado mensagem de sucesso para exportação nos locales

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -5,10 +5,9 @@ use Illuminate\Support\Facades\Route;
 
 // API de contatos
 Route::middleware(['throttle:100,1'])->group(function () {
+    // Rota específica para exportação
     Route::get('/contacts/export', [ContactController::class, 'export'])->name('contacts.export');
-    Route::get('/contacts', [ContactController::class, 'index'])->name('contact.index');
-    Route::get('/contacts/{id}', [ContactController::class, 'show'])->name('contact.show');
-    Route::post('/contacts', [ContactController::class, 'store'])->name('contact.store');
-    Route::delete('/contacts/{id}', [ContactController::class, 'destroy'])->name('contact.destroy');
-    Route::put('/contacts/{id}', [ContactController::class, 'update'])->name('contacts.update');
+
+    // Rotas agrupadas usando apiResource para CRUD
+    Route::apiResource('contacts', ContactController::class);
 });
